7a35cd4aac9fd3b4fc9bd2ab695729ace6bc6970
Ashok Kumar <kashok@vmware.com>
2020-01-28 07:22:48 +0530

    NextGreaterNumber : using the digits of given num

--- src/com/puchs/GetNextGreaterNumber.java
+++ src/com/puchs/GetNextGreaterNumber.java
@@ -13,10 +13,44 @@ public class GetNextGreaterNumber {
         if(num < 0) System.out.println("Invalid Input.");
         if(num <= 11) System.out.println("Not possible - " + num);
 
+        int[] digits = numberToDigits(num, length);
+        playWithDigits(digits, length);
+
+    }
+
+    private static void playWithDigits(int[] digits, int len) {
+        int i = len - 1;
+        while (i > 0) {
+            if(digits[i] > digits[i-1]) {
+                int temp = digits[len -1];
+                digits[len - 1] = digits[i - 1];
+                digits[i - 1] = temp;
+                break;
+            }
+            i--;
+        }
+
+        if (i == 0) {
+            System.out.println("Not possible");
+            return;
+        }
+
+        //Sort remaining digits
+        System.out.println("digits = before sort - " + Arrays.toString(digits));
+        Arrays.sort(digits, i, len -1);
+        System.out.println("digits = after sort - " + Arrays.toString(digits));
+        int[] temp = new int[len-i];
+        System.arraycopy(digits, i, temp ,0, len-i);
+        System.out.println("temp = " + Arrays.toString(temp));
+        Arrays.sort(temp);
+        System.out.println("temp after sort= " + Arrays.toString(temp));
+        System.arraycopy(temp, 0, digits, i, len - i);
+        System.out.println("digits finally = " + Arrays.toString(digits));
+        System.out.println( "NextGreaterNum is = " + digitsToNumber(digits));
     }
 
 
-    private static int digitsToNumber(Integer[] digits) {
+    private static int digitsToNumber(int[] digits) {
         int n = 0;
         for (int i = 0; i < digits.length; i++) {
             n = n * 10 + digits[i];
@@ -24,13 +58,13 @@ public class GetNextGreaterNumber {
         return n;
     }
 
-    private static Integer[] numberToDigits(int num, int len) {
-        Integer[] digits = new Integer[len];
-        int i = 0;
+    private static int[] numberToDigits(int num, int len) {
+        int[] digits = new int[len];
+        int i = len - 1;
         while(num > 0) {
             digits[i] = num % 10;
             num /= 10;
-            i++;
+            i--;
         }
         System.out.println(Arrays.toString(digits));
         return digits;
