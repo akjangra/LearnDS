24425c5040ba7ddd88b1e4bf5964b87d6efc5f17
Ashok Kumar <kashok@vmware.com>
2020-01-29 06:17:40 +0530

    BinarySearch sortedArr, binarySearchLeftMost - sortedArr with duplicates

--- src/com/puchs/BinarySearchTechniques.java
+++ src/com/puchs/BinarySearchTechniques.java
@@ -15,8 +15,14 @@ public class BinarySearchTechniques {
         System.out.println("Enter num to search in arr = ");
         Scanner in = new Scanner(System.in);
         int num = Integer.parseInt(in.nextLine());
-
         binarySearch(sortedArr, num);
+
+        int[] sortedArrWithDuplicates = {1, 2, 2, 2, 2, 7, 7, 7, 7, 7, 7, 7, 7, 7, 15, 17, 19, 21, 23, 26, 29, 31, 34, 43, 91};
+        System.out.println("Given sorted arr = " + Arrays.toString(sortedArrWithDuplicates));
+        System.out.println("Enter num to search in arr(Leftmost) = ");
+        Scanner input = new Scanner(System.in);
+        int search = Integer.parseInt(input.nextLine());
+        binarySearchLeftMost(sortedArrWithDuplicates, search);
     }
 
     private static void binarySearch(int[] arr, int num) {
@@ -32,6 +38,34 @@ public class BinarySearchTechniques {
             if (arr[mid] == num) {
                 System.out.println("Element found at index = " + mid);
                 return;
+            } else if (arr[mid] > num) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        System.out.println("Element NOT found.");
+    }
+
+
+    private static void binarySearchLeftMost(int[] arr, int num) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+
+        while(start <= end) {
+            mid = (start + end) / 2;
+            if (arr[mid] == num) {
+                //Difference start here
+                if(mid == 0 || arr[mid] > arr[mid-1]) { // For right side --> arr[mid] < arr[mid-1]
+                    System.out.println("Element found at index = " + mid);
+                    return;
+                } else {
+                    end = mid - 1; // For right side --> start = mid + 1;
+                }
+                //Difference END here
+
             } else if (arr[mid] > num) {
                 end = mid - 1;
             } else {
